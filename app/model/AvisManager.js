16ba286c94e7e1e8c70f63728858166bc3a5c020
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2021-06-03 09:48:46 +0200

    summary

--- app/model/AvisManager.js
+++ app/model/AvisManager.js
@@ -10,24 +10,6 @@ class CategoryManager{
       },  );
    }
 
-
-   get(id, callback){
-      this.db.getDB();
-      this.db.execQuery("SELECT * FROM avis WHERE id=?", [id], function(result){
-         callback(result[0]);
-      });
-   }
-
-   insert(nom){
-      this.db.getDB();
-      this.db.execQuery("INSERT INTO avis(nom) VALUES(?)", [nom]);
-   }
-
-   update(id,nom){
-      this.db.getDB();
-      this.db.execQuery("UPDATE avis SET nom=? WHERE id=?", [nom, id]);
-   }
-
    
    delete(id){
       this.db.getDB();
